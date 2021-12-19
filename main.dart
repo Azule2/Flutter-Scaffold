d027074c7b934613c5f26d080d3db98ee09b367f
Azule2 <69428406+Azule2@users.noreply.github.com>
2021-12-19 13:02:34 +0300

    main.dart
    Flutter Scaffold New Course (01)

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+home:Scaffold(
+    appBar: AppBar(
+
+     title: Text("IMine"),
+
+      centerTitle: true,
+
+    ),
+    body: Center(
+
+      child: Text("Hello IMine"),
+      
+    ),
+)
+  ));
+
+  
+}
+
+ 
